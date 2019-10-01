0ae8403d2686670965da94aec3c6a4e28899c279
Ivelin Penchev <ivelinpenchev@gmail.com>
2019-10-01 22:33:45 +0100

    Moved ConsoleWriter to svc/cli

--- go/common/result/writer.go
+++ go/common/result/writer.go
@@ -1,8 +1,6 @@
 package result
 
 import (
-	"log"
-
 	"github.com/iv-p/apid/common/transaction"
 )
 
@@ -11,25 +9,4 @@ type Writer interface {
 	Header()
 	Write(transaction.Result)
 	Footer()
-}
-
-// ConsoleWriter sends transaction results to stdout
-type ConsoleWriter struct {
-	success, failure int
-}
-
-// Header prints the header to the console
-func (w *ConsoleWriter) Header() {
-	log.Printf("Header\n")
-}
-
-// Write prints a result to the console
-func (w *ConsoleWriter) Write(tx transaction.Result) {
-	w.success++
-	log.Printf("Result\n")
-}
-
-// Footer prints the footer to the console
-func (w *ConsoleWriter) Footer() {
-	log.Printf("success: %d failure: %d\n", w.success, w.failure)
 }
--- go/svc/cli/result/writer.go
+++
@@ -1,4 +0,0 @@
-package result
-
-type Writer interface {
-}

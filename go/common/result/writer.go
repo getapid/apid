22d73b7c3fe47f92f4215314a10c47e6a24e169d
Ivelin Penchev <ivelinpenchev@gmail.com>
2019-10-01 22:30:44 +0100

    Added console result writer

---
+++ go/common/result/writer.go
@@ -0,0 +1,35 @@
+package result
+
+import (
+	"log"
+
+	"github.com/iv-p/apid/common/transaction"
+)
+
+// Writer is the interface for result writers
+type Writer interface {
+	Header()
+	Write(transaction.Result)
+	Footer()
+}
+
+// ConsoleWriter sends transaction results to stdout
+type ConsoleWriter struct {
+	success, failure int
+}
+
+// Header prints the header to the console
+func (w *ConsoleWriter) Header() {
+	log.Printf("Header\n")
+}
+
+// Write prints a result to the console
+func (w *ConsoleWriter) Write(tx transaction.Result) {
+	w.success++
+	log.Printf("Result\n")
+}
+
+// Footer prints the footer to the console
+func (w *ConsoleWriter) Footer() {
+	log.Printf("success: %d failure: %d\n", w.success, w.failure)
+}
--- go/common/transaction/transaction.go
+++ go/common/transaction/transaction.go
@@ -10,3 +10,7 @@ type Transaction struct {
 	Variables map[string]interface{} `yaml:"variables"`
 	Steps     []step.Step            `yaml:"steps"`
 }
+
+// Result holds information for the result of a transaction
+// Passed to the result writer. Content TBD
+type Result struct{}

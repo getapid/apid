7708f0b90b1b414c7d2a67fadef2f0d41ad30058
Georgi Tsaklev <georgi.tsaklev@gmail.com>
2019-10-06 12:19:16 +0100

    Restructure the validation logic

--- go/common/config/validators.go
+++ go/common/config/validators.go
@@ -17,77 +17,144 @@ func (v DefaultValidator) Validate(val interface{}) (bool, error) {
 }
 
 // StringValidator validates string presence and/or its length.
-type StringValidator struct {
-	requirements map[string]bool
-}
+type StringValidator struct{}
 
 // Validate method performs validation and returns result and optional error.
 func (v StringValidator) Validate(val interface{}) (b bool, err error) {
-	s := val.(string)
-	l := len(s)
-
-	if _, ok := v.requirements["required"]; ok && l == 0 {
-		err = multierr.Append(err, fmt.Errorf("cannot be blank"))
+	if val == nil {
+		return true, nil
 	}
 
-	if _, ok := v.requirements["version"]; ok && s != "1" {
-		err = multierr.Append(err, fmt.Errorf("supported version: \"1\""))
+	_, ok := val.(string)
+
+	if !ok {
+		return false, fmt.Errorf("must be a string")
+	}
+
+	return true, nil
+}
+
+// VersionValidator validates string if it is a valid version
+type VersionValidator struct{}
+
+// Validate method performs validation and returns result and optional error.
+func (v VersionValidator) Validate(val interface{}) (bool, error) {
+	s, ok := val.(string)
+
+	if !ok {
+		return false, fmt.Errorf("version must be a string")
+	}
+
+	if s != "1" {
+		return false, fmt.Errorf("supported versions: \"1\"")
+	}
+
+	return true, nil
+}
+
+// RecurseValidator calls the validation on each element of a slice
+type RecurseValidator struct{}
+
+// Validate method performs validation and returns result and optional error.
+func (v RecurseValidator) Validate(val interface{}) (bool, error) {
+	if val == nil {
+		return true, nil
+	}
+
+	slice := reflect.ValueOf(val)
+
+	if slice.Kind() != reflect.Slice {
+		return false, fmt.Errorf("must be a slice")
+	}
+
+	var err error
+	for i := 0; i < slice.Len(); i++ {
+		err = validateStruct(slice.Index(i).Interface(), err)
 	}
 
 	return err == nil, err
 }
 
-// SliceValidator validates slices and their length
-type SliceValidator struct {
-	requirements map[string]bool
-	unique       []string
-}
+// SliceValidator validates slices
+type SliceValidator struct{}
 
 // Validate method performs validation and returns result and optional error.
 func (v SliceValidator) Validate(val interface{}) (b bool, err error) {
 	if val == nil {
-		if _, ok := v.requirements["required"]; ok {
-			return false, fmt.Errorf("should not be empty")
-		}
+		return true, nil
 	}
 
 	slice := reflect.ValueOf(val)
 
-	if _, ok := v.requirements["required"]; ok && slice.Len() == 0 {
-		return false, fmt.Errorf("should not be empty")
+	if slice.Kind() != reflect.Slice {
+		return false, fmt.Errorf("must be a slice")
 	}
 
-	for _, uniqueField := range v.unique {
-		seen := make(map[string]bool)
-		for i := 0; i < slice.Len(); i++ {
-			valueInQuestion := slice.Index(i).FieldByName(uniqueField).String()
-			if _, ok := seen[valueInQuestion]; ok {
-				err = multierr.Append(err, fmt.Errorf("should contain unique values for %s. Found multiple %s", uniqueField, valueInQuestion))
-			}
-			seen[valueInQuestion] = true
-		}
+	return true, nil
+}
+
+// UniqueValidator validates all elements in the slice have unique property (defined by the prop)
+type UniqueValidator struct {
+	prop string
+}
+
+// Validate method performs validation and returns result and optional error.
+func (v UniqueValidator) Validate(val interface{}) (b bool, err error) {
+	if val == nil {
+		return true, nil
 	}
 
+	slice := reflect.ValueOf(val)
+
+	if slice.Kind() != reflect.Slice {
+		return false, fmt.Errorf("must be a slice")
+	}
+
+	seen := make(map[string]bool)
 	for i := 0; i < slice.Len(); i++ {
-		err = validateStruct(slice.Index(i).Interface(), nil)
+		valueInQuestion := slice.Index(i).FieldByName(v.prop).String()
+		if _, ok := seen[valueInQuestion]; ok {
+			err = multierr.Append(err, fmt.Errorf("should contain unique values for %s. Found multiple %s", v.prop, valueInQuestion))
+		}
+		seen[valueInQuestion] = true
 	}
 
 	return err == nil, err
 }
 
 // StructValidator validates recursively the struct
-type StructValidator struct {
-	requirements map[string]bool
-}
+type StructValidator struct{}
 
 // Validate method performs validation and returns result and optional error.
-func (v StructValidator) Validate(val interface{}) (b bool, err error) {
+func (v StructValidator) Validate(val interface{}) (bool, error) {
 	if val == nil {
-		if _, ok := v.requirements["required"]; ok {
-			return false, fmt.Errorf("should not be empty")
-		}
+		return true, nil
 	}
 
-	err = validateStruct(val, nil)
+	struc := reflect.ValueOf(val)
+
+	if struc.Kind() != reflect.Struct {
+		return false, fmt.Errorf("must be a struct")
+	}
+
+	err := validateStruct(val, nil)
 	return err == nil, err
+}
+
+// RequiredValidator validates slices and their length
+type RequiredValidator struct{}
+
+// Validate method performs validation and returns result and optional error.
+func (v RequiredValidator) Validate(val interface{}) (b bool, err error) {
+	if val == nil {
+		return false, fmt.Errorf("must not be nil")
+	}
+
+	thing := reflect.ValueOf(val)
+
+	if thing.Kind() != reflect.Struct && thing.Len() == 0 {
+		return false, fmt.Errorf("length must not be 0")
+	}
+
+	return true, nil
 }
--- go/common/step/step.go
+++ go/common/step/step.go
@@ -2,16 +2,16 @@ package step
 
 // Step is the data for a single endpoint
 type Step struct {
-	ID        string                 `yaml:"id" validate:"required"`
+	ID        string                 `yaml:"id" validate:"string,required"`
 	Variables map[string]interface{} `yaml:"variables"`
-	Request   Request                `yaml:"request" validate:"required"`
+	Request   Request                `yaml:"request" validate:"struct,required"`
 	Response  ExpectedResponse       `yaml:"response"`
 }
 
 // Request is a single step request data
 type Request struct {
-	Type     string  `yaml:"type" validate:"required"`
-	Endpoint string  `yaml:"endpoint" validate:"required"`
+	Type     string  `yaml:"type" validate:"string,required"`
+	Endpoint string  `yaml:"endpoint" validate:"string,required"`
 	Headers  Headers `yaml:"headers"`
 	Body     string  `yaml:"body"`
 }

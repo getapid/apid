29d7fb4f34979db319cd68e1aed0ab13b3edd1d4
Dimitar Dimitrov <donkorleone20@gmail.com>
2019-10-09 23:20:09 +0100

    Implement result.multiWriter

--- go/common/result/writer.go
+++ go/common/result/writer.go
@@ -8,3 +8,23 @@ type Writer interface {
 	Write(transaction.Result)
 	Close()
 }
+
+type multiWriter struct {
+	writers []Writer
+}
+
+func NewMultiWriter(w ...Writer) Writer {
+	return multiWriter{writers: w}
+}
+
+func (w multiWriter) Write(result transaction.Result) {
+	for _, writer := range w.writers {
+		writer.Write(result)
+	}
+}
+
+func (w multiWriter) Close() {
+	for _, writer := range w.writers {
+		writer.Close()
+	}
+}

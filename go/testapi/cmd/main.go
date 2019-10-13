ba10201d5ea9288c2f85657d2f8a5804d214ca45
Dimitar Dimitrov <donkorleone20@gmail.com>
2019-10-13 17:38:17 +0100

    Add Makefile, Dockerfile and docker-compose for test-api

--- go/testapi/cmd/main.go
+++ go/testapi/cmd/main.go
@@ -11,7 +11,7 @@ import (
 )
 
 const (
-	listenAddr = "localhost:8080"
+	listenAddr = "0.0.0.0:8080"
 )
 
 func main() {
@@ -28,6 +28,7 @@ func main() {
 
 	go log.L.Error(server.ListenAndServe())
 	defer server.Close()
+	log.L.Info("listening on ", listenAddr)
 
 	sig := make(chan os.Signal)
 	signal.Notify(sig, os.Interrupt, os.Kill)

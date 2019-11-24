9ab57d001752f3c2d6346876948abfda80d35554
Ivelin Penchev <ivelinpenchev@gmail.com>
2019-11-24 15:41:19 +0000

    Added version injection in cli

---
+++ go/svc/cli/cmd/version.go
@@ -0,0 +1,22 @@
+package cmd
+
+import (
+	"fmt"
+
+	"github.com/spf13/cobra"
+)
+
+var version string
+
+var versionCmd = &cobra.Command{
+	Use:   "version",
+	Short: "Print the version number of Hugo",
+	Long:  `All software has versions. This is Hugo's`,
+	Run: func(cmd *cobra.Command, args []string) {
+		fmt.Printf("APId v%s\n", version)
+	},
+}
+
+func init() {
+	rootCmd.AddCommand(versionCmd)
+}

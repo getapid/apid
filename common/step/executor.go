2b28a3e672a379df77424202cc4f186b025a0230
Ivelin Penchev <ivelin.penchev@gmail.com>
2020-06-26 18:08:23 +0100

    Executor interface made public

--- common/step/executor.go
+++ common/step/executor.go
@@ -8,8 +8,8 @@ import (
 	"github.com/getapid/apid-cli/common/log"
 )
 
-type executor interface {
-	do(Request) (*http.Response, error)
+type Executor interface {
+	Do(Request) (*http.Response, error)
 }
 
 type httpExecutor struct {
@@ -17,11 +17,11 @@ type httpExecutor struct {
 }
 
 // NewHTTPExecutor instantiates a new http executor
-func NewHTTPExecutor(client http.Client) executor {
+func NewHTTPExecutor(client http.Client) Executor {
 	return &httpExecutor{client: client}
 }
 
-func (e *httpExecutor) do(request Request) (*http.Response, error) {
+func (e *httpExecutor) Do(request Request) (*http.Response, error) {
 	req, err := http.NewRequest(request.Type, request.Endpoint, strings.NewReader(request.Body))
 	if err != nil {
 		return nil, err

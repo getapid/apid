003b5b5fa947ceff2e5e9f54573cb564a95b5eeb
Ivelin Penchev <ivelin.penchev@gmail.com>
2020-06-26 20:47:03 +0100

    Added remote step executor

---
+++ common/step/remote_executor.go
@@ -0,0 +1,69 @@
+package step
+
+import (
+	"bytes"
+	"io/ioutil"
+	native "net/http"
+	"os"
+
+	"github.com/getapid/apid-cli/common/log"
+
+	"github.com/getapid/apid-cli/common/http"
+	"gopkg.in/yaml.v2"
+)
+
+var endpoints = map[string]string{
+	"us-east": "https://api.getapid.com/step/executor",
+}
+
+type remoteHTTPExecutor struct {
+	key      string
+	endpoint string
+	c        *native.Client
+}
+
+// NewRemoteHTTPExecutor instantiates a new http executor
+func NewRemoteHTTPExecutor(client *native.Client, apiKey string, region string) Executor {
+	endpoint, ok := endpoints[region]
+	if !ok {
+		log.L.Errorf("invalid region selected - %s", region)
+		os.Exit(1)
+	}
+	return &remoteHTTPExecutor{
+		c:        client,
+		endpoint: endpoint,
+		key:      apiKey,
+	}
+}
+
+func (e *remoteHTTPExecutor) Do(request Request) (*http.Response, error) {
+	body, err := yaml.Marshal(request)
+	if err != nil {
+		return nil, err
+	}
+
+	req, err := native.NewRequest("POST", e.endpoint, bytes.NewReader(body))
+	if err != nil {
+		return nil, err
+	}
+
+	req.Header.Set("Authorization", e.key)
+	res, err := e.c.Do(req)
+	if err != nil {
+		return nil, err
+	}
+
+	respBody, err := ioutil.ReadAll(res.Body)
+	if err != nil {
+		return nil, err
+	}
+
+	res.Body.Close()
+	result := &http.Response{}
+	err = yaml.Unmarshal(respBody, result)
+	if err != nil {
+		return nil, err
+	}
+
+	return result, nil
+}
